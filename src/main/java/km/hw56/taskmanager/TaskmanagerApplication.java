05331422492e1d686b44aeb8509dc1537dbd90fa
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-27 16:08:52 +0600

    first commit

---
+++ src/main/java/km/hw56/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package km.hw56.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
+
+}
