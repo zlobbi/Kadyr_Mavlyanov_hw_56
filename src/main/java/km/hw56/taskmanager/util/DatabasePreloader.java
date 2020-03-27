afeaa6269ace930276bce0390c4729948d74276c
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-27 20:46:58 +0600

    added data preloader

---
+++ src/main/java/km/hw56/taskmanager/util/DatabasePreloader.java
@@ -0,0 +1,51 @@
+package km.hw56.taskmanager.util;
+
+import km.hw56.taskmanager.model.*;
+import km.hw56.taskmanager.repository.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+@Configuration
+public class DatabasePreloader {
+    private Random r = new Random();
+
+    @Bean
+    CommandLineRunner generateGibberish(UserRepository usersRepo, TaskRepository taskRepo) {
+        return args -> {
+            // users repo initialization
+            usersRepo.deleteAll();
+            taskRepo.deleteAll();
+
+            var users = Stream.generate(User::make).limit(10).collect(toList());
+
+            // tasks repository initialization
+            int i = 0;
+            List<Task> tasks = new ArrayList<>();
+            while (i < 30) {
+                var u = users.get(r.nextInt(users.size()));
+                var t = Task.make(u);
+                tasks.add(t);
+                i++;
+            }
+
+            usersRepo.saveAll(users);
+            taskRepo.saveAll(tasks);
+
+            System.out.println("\nUsers:");
+            usersRepo.findAll().forEach(u -> System.out.println(u));
+            System.out.println("\nTasks:");
+            AtomicInteger pi = new AtomicInteger(1);
+            taskRepo.findAll().forEach(pu -> System.out.println((pi.getAndIncrement()) + " " + pu.toString()));
+
+        };
+    }
+}
